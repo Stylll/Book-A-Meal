10ab92938e4e3239defa06facc253ab0f0d90e51
Styll <stephen.aribaba@yahoo.com>
2018-04-19 01:59:55 +0100

    Created modal popup to handle delete confirmation on meal pages

---
+++ UI/js/modal.js
@@ -0,0 +1,17 @@
+/**
+  Js script to handle modal popups and close
+ */
+
+// get the modal element
+const modal = document.getElementById('modal');
+
+// function to popup modal box
+const modalPopup = () => {
+  modal.style.display = 'block';
+};
+
+// function to close modal box
+const modalClosePopup = () => {
+  modal.style.display = 'none';
+};
+
