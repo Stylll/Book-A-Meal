d60fa47cc2b272ab0cf9b07f39837428452d15a1
Styll <stephen.aribaba@yahoo.com>
2018-04-20 02:41:43 +0100

    rebuilt navigation bar to become responsive

--- UI/js/script.js
+++ UI/js/script.js
@@ -7,8 +7,23 @@ const redirectTo = location => {
 }
 
 /**
+* Navbar menu trigger on small screen
+ */
+const expandNav = () => {
+  const nav = document.getElementsByTagName('nav')[0];
+  if (nav.className.indexOf('responsive') == -1) {
+    nav.className += ' responsive';
+  }
+  else{
+    const index = nav.className.indexOf('responsive');
+    nav.className = nav.className.substring(0, index);
+  }
+}
+
+/**
  * Document is ready function
  */
 $(document).ready(function(){
   $(".datepicker").datepicker();
 });
+
