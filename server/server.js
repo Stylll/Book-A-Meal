61d678e955286694585e9cd402b6f596a1b0ab05
Styll <stephen.aribaba@yahoo.com>
2018-04-23 14:37:24 +0100

    Setup basic server route and version route flow [Finishes #156991713]

---
+++ server/server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+import open from 'open';
+
+import routes from './routes';
+
+/* eslint-disable no-console */
+
+const port = process.env.PORT || 3000;
+const app = express();
+const address = `http://localhost:${port}/api/v1`;
+
+// Middleware
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Pass api/v1 requests to routes
+app.use('/api/v1', routes);
+
+// Handle 404 errors and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('404 not found');
+  err.status = 404;
+  next(err);
+});
+
+// Error Handler
+app.use((err, req, res) => {
+  res.status(err.status || 500).send(err.message || 'Error');
+});
+
+// Listen at designated port
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    open(address);
+  }
+});
+
+export default app;
