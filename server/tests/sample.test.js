6dcb2fa004ef35d4350e6499ab9da2b57b0f3de5
Styll <stephen.aribaba@yahoo.com>
2018-04-23 13:25:29 +0100

    Create and test sample test suite [Finishes #156991684]

---
+++ server/tests/sample.test.js
@@ -0,0 +1,16 @@
+import {expect} from 'chai';
+/**
+ * Sample test suite
+ */
+/*
+describe("Sample test to ensure test framework works", () => {
+  it('should pass', () => {
+    expect(1).to.equal(1);
+  });
+
+  it('should fail', () => {
+    expect(2).to.equal(1);
+  });
+});
+*/
+
