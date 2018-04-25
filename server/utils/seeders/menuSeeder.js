9e3148f24290dd4bc9324ee37fb72e873010c501
Styll <stephen.aribaba@yahoo.com>
2018-04-25 09:20:54 +0100

    Create Menu Seeder object and date beautifier function [#156922499]

---
+++ server/utils/seeders/menuSeeder.js
@@ -0,0 +1,29 @@
+/**
+ * This script contains template menu data to use for test.
+ */
+const validMenu1 = {
+  name: 'Menu For Monday, 23 April 2018',
+  date: '2018-04-23',
+};
+
+const validMenu2 = {
+  name: 'Menu For Tuesday, 24 April 2018',
+  date: '2018-04-24',
+};
+
+const validMenu3 = {
+  name: 'Menu For Wednesday, 25 April 2018',
+  date: '2018-04-25',
+};
+
+const existingMenu = {
+  name: 'Menu For Thursday, 26 April 2018',
+  date: '2018-04-26',
+};
+
+export {
+  validMenu1,
+  validMenu2,
+  validMenu3,
+  existingMenu,
+};
