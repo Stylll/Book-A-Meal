ab0969e67c73814b65d01c8a4a4c0a9188ef8af8
Styll <stephen.aribaba@yahoo.com>
2018-04-25 21:49:14 +0100

    Add middleware validation for meal menu post [#156922499]

--- server/middlewares/menus/validateMenu.js
+++ server/middlewares/menus/validateMenu.js
@@ -1,4 +1,6 @@
 import menuMeals from '../../db/menuMeals';
+import menus from '../../db/menus';
+import meals from '../../db/meals';
 
 /**
  * Middleware class to validate Meal
@@ -11,7 +13,40 @@ class ValidateMenu {
    * @param {*} next
    */
   static postMeal(req, res, next) {
+    // check if menu id is provided
+    if (!req.params.id) return res.status(400).send({ message: 'Menu id is required' });
 
+    // check if menu id is valid
+    if (!Number.isInteger(req.params.id)) {
+      return res.status(400).send({ message: 'Menu id is invalid' });
+    }
+
+    // check if menu id exists
+    if (!menus.get(req.params.id)) return res.status(400).send({ message: 'Menu does not exist' });
+
+    // check if meal id is provided
+    if (!req.body.mealId) return res.status(400).send({ message: 'Meal id is required' });
+
+    // check if meal id is valid
+    if (!Number.isInteger(req.body.mealId)) {
+      return res.status(400).send({ message: 'Meal id is invalid' });
+    }
+
+    // check if meal id exists in meals db
+    if (!meals.get(req.body.mealId)) return res.status(400).send({ message: 'Meal does not exist' });
+
+    // check if meal id exists in menumeals db
+    const result = menuMeals.getByMenuId(req.params.id);
+    if (result) {
+      // get meal using mealId if exists
+      const meal = result.filter(x => x.mealId === req.body.mealId);
+      if (meal.length > 0) {
+        // means the meal exists in the menu already
+        return res.status(409).send({ message: 'Meal already exists' });
+      }
+    }
+
+    return next();
   }
 }
 
