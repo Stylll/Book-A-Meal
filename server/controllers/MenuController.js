0e6e05e1c7191ec9c299b7fe1d6622ae375d58e4
Styll <stephen.aribaba@yahoo.com>
2018-04-26 01:15:17 +0100

    Add menu get method to controller, add route and create menuUtils function [#156930245]

--- server/controllers/MenuController.js
+++ server/controllers/MenuController.js
@@ -1,13 +1,14 @@
 import menus from '../db/menus';
 import menuMeals from '../db/menuMeals';
 import { getNormalDate } from '../utils/dateBeautifier';
+import menuUtils from '../utils/menu/menuUtils';
 
 /**
- * Controller Class to handle user meal requests
+ * Controller Class to handle user Menu requests
  */
 class MenuController {
   /**
-   * static method to handle post requests
+   * static method to handle menu post requests
    * creates new menu
    * returns menu
    * @param {*} req
@@ -46,6 +47,42 @@ class MenuController {
       return res.status(201).send({ menuMeal: newMeal });
     }
     return res.status(500).send({ message: 'Internal Server Error' });
+  }
+
+  /**
+   * Static method to handle get menu requests
+   * returns an array of menus for caterer
+   * returns an object of current day's menu for customer
+   * @param {*} req
+   * @param {*} res
+   */
+  static get(req, res) {
+    const { accountType } = req.decoded.user;
+
+    /**
+     * if accounttype is caterer,
+     * then we get all menus in the db
+     * and return them as an array
+     */
+    if (accountType === 'caterer') {
+      const menuList = { ...menus.getAll() };
+      if (menuList.length > 0) {
+        const properMenuList = menuUtils.buildMenus(menuList);
+        return res.status(200).send({ menus: properMenuList });
+      }
+      return res.status(200).send({ menus: [] });
+    }
+    /**
+     * accounttype is customer,
+     * we get the menu for current date
+     * and return it as an object
+     */
+    const menuObject = { ...menus.getByDate(new Date()) };
+    if (menuObject) {
+      const properMenuObject = menuUtils.buildMenu(menuObject);
+      return res.status(200).send({ menu: properMenuObject });
+    }
+    return res.status(404).send({ message: 'Menu for the day not set' });
   }
 }
 
---
+++ server/utils/menu/menuUtils.js
@@ -0,0 +1,50 @@
+import meals from '../../db/meals';
+import menuMeals from '../../db/menuMeals';
+
+/**
+ * Class to implement functions related
+ * to menu objects and requests
+ */
+
+class MenuUtils {
+  /**
+   * Static method to build complete menu
+   * add meal objects to the menu
+   * @param {object} menuObject
+   * @returns {object} complete menu
+   */
+  static buildMenu(menuObject) {
+    const menu = { ...menuObject };
+    menu.meals = [];
+    // get the list of set meal for the menu
+    const menuMealArray = menuMeals.getByMenuId(menu.id);
+
+    if (menuMealArray && menuMealArray.length > 0) {
+      /**
+       * for each meal list,
+       * get actual meal object
+       * push it into the menu.meals array
+       */
+      menuMealArray.forEach((m) => {
+        const meal = meals.get(m.id);
+        if (meal) menu.meals.push(meal);
+      });
+    }
+
+    return menu;
+  }
+
+  /**
+   * Static method to build complete menus in an array
+   * calls buildMenu method with each item in the array
+   * @param {array} menuArray
+   * @returns {array} completeMenuArray
+   */
+  static buildMenus(menuArray) {
+    const menuList = [...menuArray];
+    menuList.map(menu => this.buildMenu(menu));
+    return menuList;
+  }
+}
+
+export default MenuUtils;
