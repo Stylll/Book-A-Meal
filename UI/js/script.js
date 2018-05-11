13e93ab52acecb7f1b8160bb679dc5e584a75f76
Styll <stephen.aribaba@yahoo.com>
2018-05-11 03:59:22 +0100

    Implement feedback on UI

--- UI/js/script.js
+++ UI/js/script.js
@@ -21,9 +21,39 @@ const expandNav = () => {
 }
 
 /**
+ * Handler for navigation dropdown menu
+ * This toggles the dropdown content of a navigation item
+ */
+const toggleNav = (link) => {
+  const item = link.nextElementSibling;
+  if (item.className.indexOf('show') == -1) {
+    item.className = `${item.className} show`;
+  }
+  else {
+    item.className  = 'dropdown-content';
+  }
+  return false;
+}
+
+/**
+ * Handler for closing navigation dropdown
+ * Listens for clicks outside dropdown and closes it
+ */
+window.onclick = () => {
+  if (!event.target.matches('.dropdown-content')) {
+    listOfDropdowns = document.getElementsByClassName('.dropdown-content');
+
+    for (let i = 0; i < listOfDropdowns.length; i++) {
+      listOfDropdowns[i].className = 'dropdown-content'
+    }
+  }
+}
+
+/**
  * Document is ready function
  */
 $(document).ready(function(){
   $(".datepicker").datepicker();
 });
+
 
