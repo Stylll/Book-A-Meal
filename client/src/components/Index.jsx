6f50504f0b472e3f90148157289c674c6d349c7d
Styll <stephen.aribaba@yahoo.com>
2018-05-21 21:44:54 +0100

    Create navbar, main and index components [#157723874]

---
+++ client/src/components/Index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Main from './Main';
+
+const Index = () => {
+  return (
+    <Main>
+      <div className="main">
+      <div className="container text-center white-text level-one">
+        <div className="content">
+          <h1 className="title">Book-A-Meal</h1>
+          <p className="sub-text">Your online restaurant to find the finest cuisines</p>
+          <h3>BAM is an application where customers can make food orders and caterers 
+            can know what a customer whats to eat</h3>
+          <h3>We create a platform for customers to enjoy classy and tasty meals 
+            while leveraging the finesse of our world className caterers who are masters 
+            of their craft
+          </h3>
+          <h3 className="normal-text"><a href="./users/signup.html" className="secondary-text-color">
+          Click here</a> to join and become our customer or caterer</h3>
+          <h3 className="normal-text">You can <a href="./users/signin.html" className="secondary-text-color">
+          sign in here </a> if you are already one of us
+          </h3>
+        </div>
+      </div>
+    </div>
+    </Main>
+  );
+};
+
+export default Index;
---
+++ client/src/components/common/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavBar = (props) => {
+  return (
+  <nav className="navbar primary-bg-color flex-display-nav">
+      <div className="navitem flexleft"><a href="./orders/order-menu.html" className="navlink logo-text">Book-A-Meal</a></div>
+      <div className="navitem"><a href="./users/signin.html" className="navlink">Sign In</a></div>
+    </nav>
+  );
+};
+
+export default NavBar;
